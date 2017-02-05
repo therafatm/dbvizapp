bc0afedc31f199d9626210051d19d32c6c8edf1f
Noah Silvera <noah.a.silvera@gmail.com>
2017-02-04 16:27:08 -0800

    SOSA-24: Update go js code structure

--- js/services/goService.js
+++ js/services/goService.js
@@ -1,7 +1,86 @@
 app.service('goService', function() {
 
-	this.drawSchema = function(){
-	    var GO = go.GraphObject.make;
+	var GO = go.GraphObject.make;
+
+	 // define several shared Brushes for drawing go elements
+  var bluegrad = GO(go.Brush, "Linear", { 0: "rgb(150, 150, 250)", 0.5: "rgb(86, 86, 186)", 1: "rgb(86, 86, 186)" });
+  var greengrad = GO(go.Brush, "Linear", { 0: "rgb(158, 209, 159)", 1: "rgb(67, 101, 56)" });
+  var redgrad = GO(go.Brush, "Linear", { 0: "rgb(206, 106, 100)", 1: "rgb(180, 56, 50)" });
+  var yellowgrad = GO(go.Brush, "Linear", { 0: "rgb(254, 221, 50)", 1: "rgb(254, 182, 50)" });
+  var lightgrad = GO(go.Brush, "Linear", { 1: "#E6E6FA", 0: "#FFFAF0" });
+
+	// the template for each attribute in a node's array of item data
+  var attributeTemplate =
+    GO(go.Panel, "Horizontal",
+      GO(go.Shape,
+        { desiredSize: new go.Size(10, 10) },
+        new go.Binding("figure", "figure"),
+        new go.Binding("fill", "color")),
+      GO(go.TextBlock,
+        { stroke: "#333333",
+          font: "bold 14px sans-serif" },
+        new go.Binding("text", "name"))
+    );
+
+  // define the Node template, representing an entity
+  var tableTemplate =
+    GO(go.Node, "Auto",  // the whole node panel
+      { selectionAdorned: true,
+        resizable: true,
+        layoutConditions: go.Part.LayoutStandard & ~go.Part.LayoutNodeSized,
+        fromSpot: go.Spot.AllSides,
+        toSpot: go.Spot.AllSides,
+        isShadowed: true,
+        shadowColor: "#C5C1AA" },
+      new go.Binding("location", "location").makeTwoWay(),
+      // define the node's outer shape, which will surround the Table
+      GO(go.Shape, "Rectangle",
+        { fill: lightgrad, stroke: "#756875", strokeWidth: 3 }),
+      GO(go.Panel, "Table",
+        { margin: 8, stretch: go.GraphObject.Fill },
+        GO(go.RowColumnDefinition, { row: 0, sizing: go.RowColumnDefinition.None }),
+        // the table header
+        GO(go.TextBlock,
+          {
+            row: 0, alignment: go.Spot.Center,
+            margin: new go.Margin(0, 14, 0, 2),  // leave room for Button
+            font: "bold 16px sans-serif"
+          },
+          new go.Binding("text", "key")),
+        // the collapse/expand button
+        GO("PanelExpanderButton", "LIST",  // the name of the element whose visibility this button toggles
+          { row: 0, alignment: go.Spot.TopRight }),
+        // the list of Panels, each showing an attribute
+        GO(go.Panel, "Vertical",
+          {
+            name: "LIST",
+            row: 1,
+            padding: 3,
+            alignment: go.Spot.TopLeft,
+            defaultAlignment: go.Spot.Left,
+            stretch: go.GraphObject.Horizontal,
+            itemTemplate: attributeTemplate
+          },
+          new go.Binding("itemArray", "items"))
+      )  // end Table Panel
+    );  // end Node
+
+  // define the Link template, representing a relationship
+  var relationshipTemplate =
+		  GO(go.Link,  // the whole link panel
+			{
+				selectionAdorned: true,
+				layerName: "Foreground",
+				reshapable: true,
+				routing: go.Link.AvoidsNodes,
+				corner: 5,
+				curve: go.Link.JumpOver
+			},
+			GO(go.Shape,  // the link shape
+				{ stroke: "#303B45", strokeWidth: 2.5 })
+		);
+
+	this.drawSchema = () => {
 	    var diagram =
 	        GO(go.Diagram, "databaseDiagram",
 	            {
@@ -12,79 +91,41 @@ app.service('goService', function() {
 	                layout: GO(go.ForceDirectedLayout)
 	            });
 
-	    // Visualize a database; based on http://gojs.net/latest/samples/entityRelationship.html
+    diagram.nodeTemplate = tableTemplate
+		diagram.linkTemplate = relationshipTemplate
 
-	    // Database column
-	    var colTempl = GO(go.Panel, "Horizontal", GO(go.TextBlock,
-	        {}, new go.Binding("text", "name")));
-
-	    var tableTempl = GO(go.Node, "Auto", {}, new go.Binding("location", "location").makeTwoWay(),
-	        GO(go.Shape, "Rectangle", {fill: "#FFFAF0"}),
-	        GO(go.Panel, "Table", {},
-	            GO(go.RowColumnDefinition, {row: 0}),
-	            GO(go.TextBlock, {font: "bold 16px sans-serif"}, new go.Binding("text", "key")),
-	            GO(go.Panel, "Vertical", {name: "LIST", row: 1, itemTemplate: colTempl}, new go.Binding("itemArray", "items"))
-	        ) // end table panel
-	    ); // end node
-
-	    diagram.nodeTemplate = tableTempl;
-
-	    // Link template (relationship)
-	    var linkTempl = GO(go.Link, {reshapable: true, routing: go.Link.AvoidsNodes, curve: go.Link.JumpOver},
-	                    GO(go.Shape, {}),
-	                    // "from" label.
-	                    GO(go.TextBlock, {segmentOffset: new go.Point(NaN, NaN), segmentIndex: 0}, new go.Binding("text", "fromText")),
-	                    // "to" label.
-	                    GO(go.TextBlock, {segmentOffset: new go.Point(NaN, NaN), segmentIndex: -1}, new go.Binding("text", "toText"))
-	    ); // end link
-
-	    diagram.linkTemplate = linkTempl;
-
-	    // What we will have: the result of a pair of SQL queries
-	    // Representation
-	    var query1 = [
-	        { table_name: "students", column_name: "student_id", column_key: "PRI" },
-	        { table_name: "students", column_name: "first_name", column_key: "" },
-	        { table_name: "students", column_name: "last_name", column_key: "" },
-	        { table_name: "students", column_name: "program_id", column_key: "MUL" },
-	        { table_name: "students", column_name: "school_id", column_key: "MUL" },
-	        { table_name: "programs", column_name: "program_id", column_key: "PRI" },
-	        { table_name: "programs", column_name: "program_name", column_key: "" },
-	        { table_name: "schools", column_name: "school_id", column_key: "PRI" },
-	        { table_name: "schools", column_name: "school_name", column_key: ""	}
-	    ];
-
-	    // convert to node data array
-	    var nodeDataArray = [];
-
-	    for (var i = 0; i < query1.length; i++) {
-	        var tbl_name = query1[i].table_name;
-	        var existing_tbl = _.where(nodeDataArray, {key: tbl_name});
-
-	        if (existing_tbl && existing_tbl.length > 0) {
-	            existing_tbl[0].items.push({name: query1[i].column_name, isKey: (query1[i].column_key == "PRI")});
-	        } else {
-	            var new_tbl = {key: tbl_name, items: [ {name: query1[i].column_name, isKey: (query1[i].column_key == "PRI")}]};
-	            nodeDataArray.push(new_tbl);
-	        }
-	    }
-
-	    var query2 = [
-	        { constraint_name: "Program", table_name: "students", column_name: "program_id",
-	            referenced_table_name: "programs", referenced_column_name: "program_id" },
-	        { constraint_name: "School", table_name: "students", column_name: "school_id",
-	            referenced_table_name: "schools", referenced_column_name: "school_id"}
-	    ];
-
-	    var linkDataArray = [
-	        //{ from: "students", to: "schools", fromText: "0..N", toText: "1" },
-	    ];
-
-	    for (var j = 0; j < query2.length; j++) {
-	        linkDataArray.push({from: query2[j].table_name, to: query2[j].referenced_table_name,
-	            fromText: query2[j].constraint_name, toText: "blah"});
-	    }
-
-	    diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+		diagram.model = new go.GraphLinksModel(this.fakeData.nodeDataArray, this.fakeData.linkDataArray);
 	};
+
+  // FAKE DATA
+  this.fakeData = {};
+    // create the model for the E-R diagram
+  this.fakeData.nodeDataArray = [
+  { key: "Products",
+    items: [ { name: "ProductID", iskey: true, figure: "Decision", color: yellowgrad },
+        { name: "ProductName", iskey: false, figure: "Cube1", color: bluegrad },
+        { name: "SupplierID", iskey: false, figure: "Decision", color: "purple" },
+        { name: "CategoryID", iskey: false, figure: "Decision", color: "purple" } ] },
+  { key: "Suppliers",
+    items: [ { name: "SupplierID", iskey: true, figure: "Decision", color: yellowgrad },
+        { name: "CompanyName", iskey: false, figure: "Cube1", color: bluegrad },
+        { name: "ContactName", iskey: false, figure: "Cube1", color: bluegrad },
+        { name: "Address", iskey: false, figure: "Cube1", color: bluegrad } ] },
+  { key: "Categories",
+    items: [ { name: "CategoryID", iskey: true, figure: "Decision", color: yellowgrad },
+        { name: "CategoryName", iskey: false, figure: "Cube1", color: bluegrad },
+        { name: "Description", iskey: false, figure: "Cube1", color: bluegrad },
+        { name: "Picture", iskey: false, figure: "TriangleUp", color: redgrad } ] },
+  { key: "Order Details",
+    items: [ { name: "OrderID", iskey: true, figure: "Decision", color: yellowgrad },
+        { name: "ProductID", iskey: true, figure: "Decision", color: yellowgrad },
+        { name: "UnitPrice", iskey: false, figure: "MagneticData", color: greengrad },
+        { name: "Quantity", iskey: false, figure: "MagneticData", color: greengrad },
+        { name: "Discount", iskey: false, figure: "MagneticData", color: greengrad } ] },
+  ];
+  this.fakeData.linkDataArray = [
+    { from: "Products", to: "Suppliers" },
+    { from: "Products", to: "Categories" },
+    { from: "Order Details", to: "Products" }
+  ];
 });

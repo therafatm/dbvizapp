0b00bb6780f90e5249ba14299f974a20cccb075b
Noah-Silvera <noah.a.silvera@gmail.com>
2017-02-27 16:20:29 -0800

    add template data structure

---
+++ js/services/AlgorithmOutputTemplate.json.js
@@ -0,0 +1,19 @@
+// primary key data structure
+{
+  table: "TableId",
+  primaryKey: [col1, col2, col3,...] // represents the attributes of the primary key of the table
+}
+
+
+// the data structure for the abstract entities
+{
+  primaryKeys:[ PK1, PK2, PK3, ...], // represents the different sets of primary keys in this entity
+  name : "AbstractEntityID"
+}
+
+// a abstract relationship structure
+{
+  primaryKeys: [PK1, PK2, PK3, ...], // the different sets of primary keys in this relationship 
+  name: "AbstractRelationshipID",
+  endpoints: { "Id1", "Id2"} // a tuple representing the endpoints of the relationship. Should be two abstract entities
+}

6eaaff640d3b92510145a1c239042565fbad8669
rtullybarr <rtullybarr@gmail.com>
2017-03-01 16:27:18 -0800

    Finishing up translation of algorithm into javascript: step 4

--- js/app.js
+++ js/app.js
@@ -17,6 +17,32 @@ app.config(function($routeProvider, $compileProvider) {
         });
 });
 
+// returns true iff for each item in set1, no equal item is in set 2.
+// set1, set2 are arrays
+var areDisjoint = function(set1, set2) {
+    for (let i = 0; i < set1.size; i++) {
+        for (let j = 0; j < set2.size; j++) {
+            if (set1[i] == set2[j]) return false;
+        }
+    }
+
+    return true;
+}
+
+var areEqual = function(set1, set2) {
+    for (let i = 0; i < set1.size; i++) {
+        var found = false;
+        for (let j = 0; j < set2.size; j++) {
+            if (set1[i] == set2[j]) {
+                found = true;
+            }
+        }
+        if (found == false) return false;
+    }
+
+    return true;
+}
+
 // Abstraction algorithm. Should move somewhere more suitable.
 var abstractionAlgorithm = function(relations) {
     // relations: a set of relations with their primary keys defined
@@ -34,6 +60,8 @@ var abstractionAlgorithm = function(relations) {
     // add the first relation to the abstract entities array.
     abstractEntities.push([relations[0]]);
 
+    // abstractEntities' primary key is the union of all pks of relations in it.
+
     var remainingRels = relations.copy();
     for (let i = 1; i < relations.size; i++) {
         if (relations[i].primaryKey == relations[i - 1].primaryKey) {
@@ -46,13 +74,16 @@ var abstractionAlgorithm = function(relations) {
             // Check if this is disjoint with respect to existing abstract entities.
             for (let j = 0; j < abstractEntities.size; j++) {
                 // disjoint: primary keys share no attributes.
-                for (let k = 0; j < abstractEntities[j][0].primaryKey.size; k++) {
-                    for (let l = 0; l < relations[i].primaryKey.size; l++) {
-                        if (abstractEntities[j][0].primaryKey[k] == relations[i].primaryKey[l]) {
-                            disjoint = false;
-                        }
-                    }
+                if (!areDisjoint(abstractEntities[j].primaryKey, relations[i].primaryKey)) {
+                    disjoint = false;
                 }
+                // for (let k = 0; j < abstractEntities[j][0].primaryKey.size; k++) {
+                //     for (let l = 0; l < relations[i].primaryKey.size; l++) {
+                //         if (abstractEntities[j][0].primaryKey[k] == relations[i].primaryKey[l]) {
+                //             disjoint = false;
+                //         }
+                //     }
+                // }
             }
 
             // if disjoint, create new abstract entity.
@@ -91,15 +122,20 @@ var abstractionAlgorithm = function(relations) {
         }
     }
 
-    // Finally, cluster the remaining relationships to form abstract relationships.
+    // Finally, cluster the remaining tables to form abstract relationships.
     var firstRel = true;
-    for (let i = 0; i < remainingRels.size; i++) {
+    // For each R in remainingRels:
+    relations = remainingRels.copy();
+    for (let i = 0; i < relations.size; i++) {
         var intersections = [];
+
+        // for each abstract entity
         for (let j = 0; j < abstractEntities.size; j++) {
             for (let k = 0; j < abstractEntities[j][0].primaryKey.size; k++) {
                 for (let l = 0; l < relations[i].primaryKey.size; l++) {
-                    // If the primary keys intersect, add this to intersections.
+                    // If the primary keys share an attribute, add this to intersections.
                     if (abstractEntities[j][0].primaryKey[k] == relations[i].primaryKey[l]) {
+                        // intersections: list of abstract entities with which this relation intersects.
                         intersections.push(abstractEntities[j]);
                         break;
                     }
@@ -107,6 +143,40 @@ var abstractionAlgorithm = function(relations) {
             }
         }
 
-        // Step 4 not finished.
+        if (firstRel) {
+            // indicate relationships
+            var abstractRel = {
+                name: "AR0",
+                entities: intersections,
+                rels: [relations[i]]
+            }
+
+            abstractRelationships.push(abstractRel);
+            // doesn't work, might not be same length.
+            remainingRels.slice(i, 1);
+        } else {
+            var found = false;
+            var j = 0;
+            while (j < abstractRelationships.size && !found) {
+                // If this relation links the same entities as a previously defined one
+                if (abstractRelationships[j].entities = intersections) {
+                    abstractRelationships[j].rels.push(relations[i]);
+                    remainingRels.slice(i, 1);
+                }
+            }
+
+            if (!found) {
+                // indicate relationships
+                abstractRel = {
+                    name: "AR0",
+                    entities: intersections,
+                    rels: [relations[i]]
+                }
+
+                abstractRelationships.push(abstractRel);
+                // doesn't work, might not be same length.
+                remainingRels.slice(i, 1);
+            }
+        }
     }
 };

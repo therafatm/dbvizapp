930faab6ca75822906a3157d256eb807b8142b63
rtullybarr <rtullybarr@gmail.com>
2017-03-02 18:02:08 -0800

    Algorithm improvements

--- js/app.js
+++ js/app.js
@@ -17,9 +17,16 @@ app.config(function($routeProvider, $compileProvider) {
         });
 });
 
+var orderTablesByPK = function(tables) {
+    // split into lists based on PK length
+    return _.sortBy(tables, function(table) {
+        return getPK(table).size;
+    });
+}
+
 // returns true iff for each item in set1, no equal item is in set 2.
 // set1, set2 are arrays
-var areDisjoint = function(set1, set2) {
+var areSetsDisjoint = function(set1, set2) {
     for (let i = 0; i < set1.size; i++) {
         for (let j = 0; j < set2.size; j++) {
             if (set1[i] == set2[j]) return false;
@@ -29,7 +36,8 @@ var areDisjoint = function(set1, set2) {
     return true;
 }
 
-var areEqual = function(set1, set2) {
+var areSetsEqual = function(set1, set2) {
+    // Everything in set 1 is in set 2
     for (let i = 0; i < set1.size; i++) {
         var found = false;
         for (let j = 0; j < set2.size; j++) {
@@ -40,14 +48,43 @@ var areEqual = function(set1, set2) {
         if (found == false) return false;
     }
 
+    // Everything in set 2 is in set 1
+    for (let i = 0; i < set2.size; i++) {
+        var found = false;
+        for (let j = 0; j < set1.size; j++) {
+            if (set2[i] == set1[j]) {
+                found = true;
+            }
+        }
+        if (found == false) return false;
+    }
+
     return true;
+}
+
+var getPK = function(table) {
+    return _.filter(table.cols, function(col) {
+        return (col.PK == true);
+    });
+}
+
+var addTable = function(abstractEntity, table) {
+    // Add table's primary key to entity's primary key.
+    abstractEntity.PK.concat(getPK(table));
+
+    // Add table's name to abstract entity tables
+    abstractEntity.table_names.push(table.name);
 }
 
 // Abstraction algorithm. Should move somewhere more suitable.
 var abstractionAlgorithm = function(relations) {
-    // relations: a set of relations with their primary keys defined
+    // relations: a set of tables with their primary keys defined.
 
-    // that is, an array of objects like: = {primaryKey: [attrs], allAttrs = [attrs]}
+    // Table structure:
+    // table = {
+    //   name: "table_name",
+    //   cols: [{name: "name1", PK = true}, {name: "name2", PK = false}]
+    // }
 
     var abstractEntities = [];
     var abstractRelationships = [];
@@ -58,72 +95,76 @@ var abstractionAlgorithm = function(relations) {
     // relations with the same primary key adjacent to each other.
 
     // add the first relation to the abstract entities array.
-    abstractEntities.push([relations[0]]);
+    var firstEntity = {
+        name: "AE0",
+        PK: [],
+        table_names: []
+    };
 
-    // abstractEntities' primary key is the union of all pks of relations in it.
+    addTable(firstEntity, relations[0]);
+    abstractEntities.push(firstEntity);
 
+    // abstractEntities' primary key is the union of all pks of tables in it.
+
+    // relations is the set on which we iterate.
+    // Remove processed tables from remainingRels.
     var remainingRels = relations.copy();
+    var numAEs = 1;
     for (let i = 1; i < relations.size; i++) {
-        if (relations[i].primaryKey == relations[i - 1].primaryKey) {
+        if (areSetsEqual(getPK(relations[i]), getPK(relations[i - 1]))) {
             // Add to current abstract entity.
-            abstractEntities[abstractEntities.size - 1].push(relations[i]);
+            addTable(abstractEntities[numAEs - 1], relations[i]);
+
             // remove from relations remaining to process.
-            remainingRels.splice(i, 1);
+            remainingRels.splice(remainingRels.indexOf(relations[i]), 1);
         } else {
             disjoint = true;
             // Check if this is disjoint with respect to existing abstract entities.
             for (let j = 0; j < abstractEntities.size; j++) {
                 // disjoint: primary keys share no attributes.
-                if (!areDisjoint(abstractEntities[j].primaryKey, relations[i].primaryKey)) {
+                if (!areSetsDisjoint(abstractEntities[j].PK, getPK(relations[i]))) {
                     disjoint = false;
                 }
-                // for (let k = 0; j < abstractEntities[j][0].primaryKey.size; k++) {
-                //     for (let l = 0; l < relations[i].primaryKey.size; l++) {
-                //         if (abstractEntities[j][0].primaryKey[k] == relations[i].primaryKey[l]) {
-                //             disjoint = false;
-                //         }
-                //     }
-                // }
             }
 
             // if disjoint, create new abstract entity.
             if (disjoint == true) {
-                abstractEntities.push([relations[i]]);
-                remainingRels.splice(i, 1);
+                var newEntity = {
+                    name: "AE" + numAEs,
+                    PK: [],
+                    table_names: []
+                };
+                addTable(newEntity, relations[i]);
+                abstractEntities.push(newEntity);
+                remainingRels.splice(remainingRels.indexOf(relations[i]), 1);
             }
         }
     }
 
     // Next, pass over remainingRels again, adding more relations.
     relations = remainingRels.copy();
-
     for (let i = 0; i < relations.size; i++) {
         // add relations[i] to a cluster if it has an attr in common with said cluster
         // and no attrs in common with any other cluster.
         var hasCommonAttr = [];
-        var noCommonAttr = [];
 
         for (let j = 0; j < abstractEntities.size; j++) {
-            for (let k = 0; j < abstractEntities[j][0].primaryKey.size; k++) {
-                for (let l = 0; l < relations[i].primaryKey.size; l++) {
-                    if (abstractEntities[j][0].primaryKey[k] == relations[i].primaryKey[l]) {
-                        hasCommonAttr.push(abstractEntities[j]);
-                    } else {
-                        noCommonAttr.push(abstractEntities[j]);
-                    }
-                }
+            if (!areSetsDisjoint(abstractEntities[j].PK, getPK(relations[i]))) {
+                hasCommonAttr.push(abstractEntities[j]);
             }
         }
 
+        // Common attrs with only one abstract entity.
         if (hasCommonAttr.size == 1) {
-            hasCommonAttr[0].push(relations[i]);
+            addTable(hasCommonAttr[0], relations[i]);
             // remove from remainingRels
-            remainingRels.slice(i, 1);
+            remainingRels.splice(remainingRels.indexOf(relations[i]), 1);
         }
     }
 
     // Finally, cluster the remaining tables to form abstract relationships.
     var firstRel = true;
+    var numRels = 0;
     // For each R in remainingRels:
     relations = remainingRels.copy();
     for (let i = 0; i < relations.size; i++) {
@@ -131,15 +172,8 @@ var abstractionAlgorithm = function(relations) {
 
         // for each abstract entity
         for (let j = 0; j < abstractEntities.size; j++) {
-            for (let k = 0; j < abstractEntities[j][0].primaryKey.size; k++) {
-                for (let l = 0; l < relations[i].primaryKey.size; l++) {
-                    // If the primary keys share an attribute, add this to intersections.
-                    if (abstractEntities[j][0].primaryKey[k] == relations[i].primaryKey[l]) {
-                        // intersections: list of abstract entities with which this relation intersects.
-                        intersections.push(abstractEntities[j]);
-                        break;
-                    }
-                }
+            if (!areSetsDisjoint(abstractEntities[j].PK, getPK(relations[i]))) {
+                intersections.push(abstractEntities[j]);
             }
         }
 
@@ -147,35 +181,43 @@ var abstractionAlgorithm = function(relations) {
             // indicate relationships
             var abstractRel = {
                 name: "AR0",
-                entities: intersections,
-                rels: [relations[i]]
+                abstract_entities: intersections,
+                PK: [],
+                table_names: []
             }
+            numRels = 1;
+
+            addTable(abstractRel, relations[i]);
 
             abstractRelationships.push(abstractRel);
-            // doesn't work, might not be same length.
-            remainingRels.slice(i, 1);
+
+            remainingRels.splice(remainingRels.indexOf(relations[i]), 1);
         } else {
             var found = false;
             var j = 0;
             while (j < abstractRelationships.size && !found) {
                 // If this relation links the same entities as a previously defined one
-                if (abstractRelationships[j].entities = intersections) {
-                    abstractRelationships[j].rels.push(relations[i]);
-                    remainingRels.slice(i, 1);
+                if (areSetsEqual(abstractRelationships[j].abstract_entities, intersections)) {
+                    addTable(abstractRelationships[j], relations[i]);
+                    remainingRels.splice(remainingRels.indexOf(relations[i]), 1);
                 }
             }
 
             if (!found) {
                 // indicate relationships
                 abstractRel = {
-                    name: "AR0",
-                    entities: intersections,
-                    rels: [relations[i]]
+                    name: "AR" + numRels,
+                    abstract_entities: intersections,
+                    PK: [],
+                    table_names: []
                 }
 
+                numRels++;
+
+                addTable(abstractRel, relations[i]);
                 abstractRelationships.push(abstractRel);
                 // doesn't work, might not be same length.
-                remainingRels.slice(i, 1);
+                remainingRels.splice(remainingRels.indexOf(relations[i]), 1);
             }
         }
     }

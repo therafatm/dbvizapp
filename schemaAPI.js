b89986b7546cafc93727d7340e58eeb3c274b28f
Noah-Silvera <noah.a.silvera@gmail.com>
2017-03-26 11:48:56 -0700

    Skeleton for retrieving and merging foreign keys into data structure

--- schemaAPI.js
+++ schemaAPI.js
@@ -18,46 +18,76 @@ router.use(function timeLog (req, res, next) {
 router.route('/').get(function (req, res, next) {
 
     var results = {};
+    
 
     console.log("in schemaAPI");
     console.log(req.query);
 
-    var connection = mysql.createConnection({
-        host: req.query.host,
-        user: req.query.username,
-        password: req.query.password,
-        database: 'information_schema',
-        port: req.query.port
-    });
+    if(req.query.sourceCodeDir !== undefined || req.query.sourceCodeDir.trim() != ""){
+        // Attempt to parse the java source code from this directory.
 
-    connection.connect(function(err) {
-        if(err) {
-            // handle connection errors.
-            console.error("Error connecting to mysql.");
-            console.error(err);
-            res.status(500).json({success: false, data: err, message: "Error connecting to MySQL server. Check your connection parameters."});
-        } else {
-            console.log("Successfully connected.");
-            connection.query(columnsQuery, [req.query.database], function(err, rows, fields) {
-                if (err) {
-                    console.log('error: ' + err);
-                    connection.end();
+        return Promise.resolve().then( (parsedKeys) => {
+            // [
+                /*{
+                    "table_name": <fk1_table_name>,
+                    "column_name": <fk1_column_name>,
+                    "referenced_table_name": <fk1_ref_col_name>,
+                    "referenced_table_name": <fk1_ref_col_name>
+                },
+                {
+                    "table_name": <fk2_table_name>,
+            ...
+            ]*/
+
+            result.foreignKeys = [];
+            result.foreignKeys.push( parsedKeys );
+
+            return ;
+        }, (err) => {
+            console.error("Could not parse foreign keys from the given directory " + req.query.sourceCodeDir);
+            return ;
+        }).then( () => {
+
+            var connection = mysql.createConnection({
+                host: req.query.host,
+                user: req.query.username,
+                password: req.query.password,
+                database: 'information_schema',
+                port: req.query.port
+            });
+            
+
+            connection.connect(function(err) {
+                if(err) {
+                    // handle connection errors.
+                    console.error("Error connecting to mysql.");
+                    console.error(err);
+                    res.status(500).json({success: false, data: err, message: "Error connecting to MySQL server. Check your connection parameters."});
                 } else {
-                    results.tablesAndCols = rows;
-                    connection.query(keysQuery, [req.query.database], function(err, rows, fields) {
+                    console.log("Successfully connected.");
+                    connection.query(columnsQuery, [req.query.database], function(err, rows, fields) {
                         if (err) {
                             console.log('error: ' + err);
                             connection.end();
                         } else {
-                            results.foreignKeys = rows;
-                            connection.end();
-                            return res.json(results);
+                            results.tablesAndCols = rows;
+                            connection.query(keysQuery, [req.query.database], function(err, rows, fields) {
+                                if (err) {
+                                    console.log('error: ' + err);
+                                    connection.end();
+                                } else {
+                                    results.foreignKeys = rows;
+                                    connection.end();
+                                    return res.json(results);
+                                }
+                            });
                         }
                     });
                 }
             });
-        }
-    });
+        })
+    }
+
 });
 
 module.exports = router;
